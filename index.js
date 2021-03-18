b432b6fb9f6211ffe793fd8a2270c321b1a52283
Matthew Norris <matthewnorris@Matthews-iMac.attlocal.net>
2021-03-18 03:33:37 -0500

    Finished first two tasks

--- index.js
+++ index.js
@@ -42,7 +42,25 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+
+    eat(someFood) {
+      if(this.stomach.length < 10) {
+        this.stomach.push(someFood)
+      }
+    }
+
+    poop() {
+      this.stomach = []
+    }
+
+    toString() {
+      return `${this.name}, ${this.age}`
+    }
   }
   
   /*
@@ -60,7 +78,27 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon) {
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+
+    fill(gallons) {
+      this.tank += gallons;
+    }
+
+    drive(distance) {
+      if(distance/this.milesPerGallon <= this.tank) {
+        this.tank -= distance/this.milesPerGallon;
+        this.odometer += distance;
+      } else if(distance/this.milesPerGallon > this.tank) {
+        this.odometer += this.tank * this.milesPerGallon;
+        this.tank = 0;
+        return `I ran out of fuel at ${this.odometer} miles!`
+      }
+    }
   }
   
   /*
